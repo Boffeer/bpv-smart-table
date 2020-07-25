bc09251956cbc4080bdcd2705b376b1082196829
boffeer <theb0ffee@gmail.com>
2020-07-25 21:58:52 +0300

    calculator begin

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
     svg4everybody({});
 
-    
+
+//calculator
+
+// ---- calculator radio check
+$('.calculator-materials-item').click(function(){
+	var target = $('#material-' + $(this).val());
+
+	$('.material-img').not(target).hide(0);
+	target.fadeIn(500);
+});
 });
 
