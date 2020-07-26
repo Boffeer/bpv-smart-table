7ba00a8df42a0e42bc621bac89bc615a9a0fcc5d
boffeer <theb0ffee@gmail.com>
2020-07-26 22:19:50 +0300

    calculator works

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,15 +1,67 @@
-$(document).ready(function () {
+$(document).ready(function(){
     svg4everybody({});
+	//calculator
+	// ready end
+});
 
+// forms
+var costSubmitted = false;
 
-//calculator
+// ---- calculator cost
+var cost = 0,
+	materialModifier = [0, 100, 200, 300, 400, 500, 600, 700, 800],
+	frameModifier = [0, 10, 20],
+	sizeModifier = [0, 1, 2, 3, 4],
+	currentMaterial = 0,
+	currentFrame = 0,
+	currentSize = 0,
+	currentCost = 0,
+	costPlace = document.querySelector('.calculator-cost__info');
+
+function getCurrentCost() {
+	return currentCost = cost + currentMaterial + currentFrame + currentSize;
+}
 
 // ---- calculator radio check
 $('.calculator-materials-item').click(function(){
-	var target = $('#material-' + $(this).val());
+	var material = $('#material-' + $(this).val()),
+			materialAr = $(this).val();
+	$('.material-img').not(material).hide(0);
+	material.fadeIn(500);
 
-	$('.material-img').not(target).hide(0);
-	target.fadeIn(500);
-});
+	// console.log(materialModifier[materialAr]);
+	// console.log(getCurrentCost())
+	return currentMaterial = materialModifier[materialAr];
 });
 
+
+$('.calculator-frame-item').click(function(){
+	let modifier = $('#frame-' + $(this).val()),
+		modifierAr = $(this).val();
+	$('.frame-img').not(modifier).hide(0);
+	modifier.fadeIn(500);
+	console.log(frameModifier[modifierAr]);
+
+	return currentFrame = frameModifier[modifierAr];
+});
+
+$('.calculator-size-item').click(function(){
+	let modifierAr = $(this).val();
+	console.log(sizeModifier[modifierAr]);
+	return currentSize = sizeModifier[modifierAr]
+});
+
+
+$('.calculator-check').click(function()
+{
+	$('.calculator-cost__info').val(getCurrentCost()).change();
+});
+
+$('.calculator-cost__info').val(materialModifier[1] +
+	frameModifier[1] + sizeModifier[1]).change();
+	
+$('.calculator-form').submit(function(e) {
+    $(':disabled').each(function(e) {
+        $(this).removeAttr('disabled');
+    })
+});
